77fad9325f6ff91cc60055fa1965f039738eddc2
Nisar Ahmed <nisar.easoblue@gmail.com>
2021-07-28 06:33:18 +0500

    fixed issue

--- resources/views/components/menu/menuItem.blade.php
+++ resources/views/components/menu/menuItem.blade.php
@@ -4,9 +4,9 @@
     'url' => "",
     'icon' => "",
 ])
-<li class="active" {{ $attributes->merge(['class' => ($active ? 'active' :'')]) }}>
+<li {{ $attributes->merge(['class' => ($active ? 'active' :'')]) }}>
     <a href="{{ $url }}">
-        <i class="icon-{{ $icon }}"></i>
+        @if($icon)<i class="icon-{{ $icon }}"></i>@endif
         <span>{{ $text }}</span>
     </a>
 </li>
